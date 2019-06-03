2d4d27f4027d66082b6a52da65a61281a254b6ab
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-03 15:49:50 -0400

    started over fml

--- house_hunting.py
+++ house_hunting.py
@@ -1,13 +1,6 @@
-annual_salary = input("Enter your annual salary: ")
-portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
-total_cost = input("Enter the cost of your dream home: ")
-
-yearly_savings = float(annual_salary) * float(portion_saved)
-monthly_savings = yearly_savings / 12
-goal = float(total_cost)
-total_months = goal / monthly_savings
-
-print("Number of months: " + str(total_months))
-
-# current savings = 100
-# interest = current_savings * r / 12
+annual_salary_as_str = input("Enter your annual salary: ")
+portion_saved_as_str = input("Enter the percent of your salary to save, as a decimal: ")
+total_cost_as_str = input("Enter the cost of your dream home: ")
+annual_salary = int(annual_salary_as_str)
+portion_saved = int(portion_saved_as_str)
+total_cost = int(total_cost_as_str)
