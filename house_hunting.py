1d8a43d05cd9af3fbb294bda907397001996f358
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-03 16:08:36 -0400

    formatting

--- house_hunting.py
+++ house_hunting.py
@@ -1,5 +1,6 @@
 annual_salary_as_str = input("Enter your annual salary: ")
-portion_saved_as_str = input("Enter the percent of your salary to save, as a decimal: ")
+portion_saved_as_str = input(
+    "Enter the percent of your salary to save, as a decimal: ")
 total_cost_as_str = input("Enter the cost of your dream home: ")
 annual_salary = int(annual_salary_as_str)
 portion_saved = int(portion_saved_as_str)
