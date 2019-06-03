07458e9be8268bfde27748a5d3cc3c8ae8d35b6d
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-03 12:45:06 -0400

    added input statements

---
+++ house_hunting.py
@@ -0,0 +1,4 @@
+annual_salary = input("Enter your annual salary: ")
+portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
+total_cost = input("Enter the cost of your dream home: ")
+
