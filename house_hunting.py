b27e7cc64958a7bfd31d8223a5b65b40c6ff8a57
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-03 13:52:19 -0400

    brain broke, cant do math, or spell

--- house_hunting.py
+++ house_hunting.py
@@ -1,5 +1,5 @@
 annual_salary = input("Enter your annual salary: ")
-portion_saved = input("Enter the cost of your dream home: ")
+portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
 total_cost = input("Enter the cost of your dream home: ")
 
 yearly_savings = float(annual_salary) * float(portion_saved)
